416af246feb7897c8d5a24ac0cf8ff05f0576303
Lars Wrenger <wrenger@sra.uni-hannover.de>
2023-01-04 13:20:45 +0100

    Extract size_counters into nvalloc module

---
+++ mm/nvalloc/include/size_counters.h
@@ -0,0 +1,32 @@
+#ifndef _NVALLOC_SIZE_COUNTERS
+#define _NVALLOC_SIZE_COUNTERS
+
+#include <linux/types.h>
+
+#ifdef CONFIG_NVALLOC_SIZE_COUNTERS
+
+void size_counters_alloc(gfp_t flags, int order);
+void size_counters_bulk_alloc(gfp_t flags, u64 inc);
+void size_counters_free(int order);
+void size_counters_bulk_free(u64 inc);
+
+#else
+static inline u64 size_counters_start(void)
+{
+	return 0;
+}
+static inline void size_counters_alloc(gfp_t flags, int order)
+{
+}
+static inline void size_counters_bulk_alloc(gfp_t flags, u64 inc)
+{
+}
+static inline void size_counters_free(int order)
+{
+}
+static inline void size_counters_bulk_free(u64 inc)
+{
+}
+#endif // CONFIG_NVALLOC_SIZE_COUNTERS
+
+#endif // _NVALLOC_SIZE_COUNTERS

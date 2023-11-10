1d3dea91c752d57f421d82a6515510401b0f363c
Lars Wrenger <wrenger@sra.uni-hannover.de>
2023-11-10 16:44:31 +0100

    Fix llfree related boot issues

--- mm/llfree/include/llfree_alloc.h
+++ mm/llfree/include/llfree_alloc.h
@@ -6,7 +6,8 @@
 #include <llfree.h>
 
 /// Create a new allocator instance for the given node
-llfree_t *llfree_node_init(size_t node, size_t cores, bool persistent, void *start, size_t pages);
+llfree_t *llfree_node_init(size_t node, size_t cores, bool persistent,
+			   u64 start_pfn, size_t pages);
 
 #endif // CONFIG_LLFREE
 #endif // _LLFREE

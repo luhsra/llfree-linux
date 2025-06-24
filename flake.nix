0d96b4e0141960337955304aa719a7fea3ecc0aa
Lars Wrenger <lars@wrenger.net>
2025-06-25 01:38:20 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,35 +1,48 @@
 {
-  description = "SRA flake for Linux kernel development";
+  description = "LLZero Linux Environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    supportedSystems = [ "aarch64-linux" "x86_64-linux"
-                         "aarch64-darwin" "x86_64-darwin" ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-  in {
-    devShells = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      default = pkgs.mkShellNoCC {
-        buildInputs = with pkgs; [
-          llvmPackages_20.clang
-          llvmPackages_20.lld
-          llvmPackages_20.llvm
-          llvmPackages_20.clang-tools
-          ncurses
-          pkg-config
-          qemu qemu_kvm qemu-utils
-          guestfs-tools
-        ];
-        inputsFrom = [ pkgs.linux ];
-        hardeningDisable = [ "all" ];
-        env = {
-          LLVM = 1;
-        };
-      };
-    });
-  };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            buildInputs = with pkgs; [
+              llvmPackages_20.clang
+              llvmPackages_20.lld
+              llvmPackages_20.llvm
+              llvmPackages_20.clang-tools
+              ncurses
+              pkg-config
+              qemu
+              qemu_kvm
+              qemu-utils
+              guestfs-tools
+            ];
+            inputsFrom = [ pkgs.linux ];
+            hardeningDisable = [ "all" ];
+            env = {
+              LLVM = 1;
+            };
+          };
+        }
+      );
+    };
 }

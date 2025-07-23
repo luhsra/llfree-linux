773b98851cd3b3a056fc3a73211b207a0c0380a4
Lars Wrenger <wrenger@sra.uni-hannover.de>
2025-07-23 15:06:46 +0200

    Merge SSD zeroing
    Co-authored-by: Pasha Fistanto <fistanto@ibr.cs.tu-bs.de>

--- mm/page_zero.c
+++ mm/page_zero.c
@@ -10,6 +10,26 @@
 #include <linux/init.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
+#include <linux/bio.h>
+#include <linux/blkdev.h>
+#include <linux/fs.h>
+#include <linux/string.h>
+#include <linux/kfifo.h>
+#include <asm-generic/ioctl.h>
+
+/// Optional size_t type
+typedef struct optional_size_t {
+	bool present : 1;
+	size_t value : (sizeof(size_t) * 8) - 1;
+} optional_size_t;
+static inline optional_size_t optional_size(size_t value)
+{
+	return (optional_size_t){ .present = true, .value = value };
+}
+static inline optional_size_t optional_size_none(void)
+{
+	return (optional_size_t){ .present = false, .value = 0 };
+}
 
 #define LLFREE_HUGE_ORDER HUGETLB_PAGE_ORDER
 #define LLZERO_ZONEMASK ((1 << ZONE_NORMAL) | (1 << ZONE_DMA32))
@@ -26,38 +46,191 @@ static inline size_t huge_page_limit(void)
 	return ((limit * 1024 * 1024) / PAGE_SIZE) >> LLFREE_HUGE_ORDER;
 }
 
+static inline size_t num_pages_to_zero(struct zone *zone)
+{
+	size_t pages_to_zero = 0;
+	ll_stats_t stats = llfree_stats(zone->llfree);
+	// Zero X% of free pages
+	if (stats.free_huge <= 8)
+		return 0;
+	pages_to_zero = stats.free_huge * percentage / 100;
+	if (pages_to_zero <= stats.zeroed_huge)
+		return 0;
+	pages_to_zero -= stats.zeroed_huge;
+
+	return pages_to_zero;
+}
+
 extern void memset_nt(void *dst, int value, size_t len);
+static int start_zero_tasks(void);
+static int stop_zero_tasks(void);
 
 static bool enabled = true;
 
 static struct task_struct **zero_tasks = NULL;
 static size_t zero_tasks_len = 0;
 
-/// Returns true if only zero percent was zeroed but there are still pages to zero.
-static bool llzero_pages(struct zone *zone)
+static char *device = NULL;
+static struct block_device *bdev = NULL;
+
+static fmode_t mode = FMODE_READ | FMODE_EXCL;
+
+struct bio_ctx {
+	struct zone *zone;
+	uint64_t frame;
+	size_t order;
+	size_t cpu;
+	size_t offset;
+	bool benchmark;
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+	ktime_t start;
+#endif
+};
+
+static int major;
+static struct class *cls;
+
+static struct bio_set bs;
+#define LLZERO_BIO_POOL_SIZE 10
+
+static atomic_t blk_2mib_ctr = ATOMIC_INIT(0);
+#define BLK_2MIB_COUNT 512
+#define SECTOR_OFFSET_2MIB 4096
+
+static atomic_t bio_inflight = ATOMIC_INIT(0);
+static uint32_t iodepth = 0;
+
+/*
+** Benchmark Stuffs
+*/
+static size_t bench_num_pages;
+static size_t bench_cpu;
+static bool bench_const_sector;
+static struct zone *bench_zone;
+static atomic_t completion_ctr = ATOMIC_INIT(0);
+static DECLARE_WAIT_QUEUE_HEAD(bench_waitq);
+
+struct benchmark_args {
+	uint32_t num_pages;
+	uint32_t iodepth;
+	bool const_iosector;
+
+	// Output
+	uint64_t duration_ns;
+};
+#define IOCTL_MAGIC 0x55
+#define ZERO_IOCTL_BENCHMARK _IOWR(IOCTL_MAGIC, 0x00, struct benchmark_args)
+
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+static void _dump_page(size_t offset, uint64_t frame)
 {
-	size_t pages_to_zero = 0;
-	ll_stats_t stats = llfree_stats(zone->llfree);
-	bool exceeds_limit = false;
-	// Zero X% of free pages
-	if (stats.free_huge <= 8)
-		return false;
-	pages_to_zero = stats.free_huge * percentage / 100;
-	if (pages_to_zero <= stats.zeroed_huge)
-		return false;
-	pages_to_zero -= stats.zeroed_huge;
+	struct page *page;
+	void *vaddr;
 
-	// Ensure we do not zero more than the limit
-	exceeds_limit = pages_to_zero > huge_page_limit();
-	pages_to_zero = min_t(size_t, pages_to_zero, huge_page_limit());
+	page = pfn_to_page(offset + frame);
+	vaddr = page_address(page);
+	pr_info("address dump: 0x%px\n", vaddr);
+	print_hex_dump(KERN_INFO, "Read Data head: ", DUMP_PREFIX_OFFSET, 16, 1,
+		       vaddr, 64, true);
+	vaddr += (511 << PAGE_SHIFT);
+	pr_info("address dump: 0x%px\n", vaddr);
+	print_hex_dump(KERN_INFO, "Read Data tail: ", DUMP_PREFIX_OFFSET, 16, 1,
+		       vaddr, 64, true);
 
-	for (size_t i = 0; i < pages_to_zero; i++) {
-		size_t offset =
-			ALIGN_DOWN(zone->zone_start_pfn, 1 << MAX_ORDER);
-		struct page *page = NULL;
-		llfree_result_t res;
+	vaddr += (1 << PAGE_SHIFT);
+	pr_info("address dump: 0x%px\n", vaddr);
+	print_hex_dump(KERN_INFO, "Read Data out-head: ", DUMP_PREFIX_OFFSET,
+		       16, 1, vaddr, 64, true);
+	vaddr += (511 << PAGE_SHIFT);
+	pr_info("address dump: 0x%px\n", vaddr);
+	print_hex_dump(KERN_INFO, "Read Data out-tail: ", DUMP_PREFIX_OFFSET,
+		       16, 1, vaddr, 64, true);
+
+	vaddr += (1 << PAGE_SHIFT);
+	pr_info("address dump: 0x%px\n", vaddr);
+	print_hex_dump(KERN_INFO, "Read Data out2-head: ", DUMP_PREFIX_OFFSET,
+		       16, 1, vaddr, 64, true);
+	vaddr += (511 << PAGE_SHIFT);
+	pr_info("address dump: 0x%px\n", vaddr);
+	print_hex_dump(KERN_INFO, "Read Data out2-tail: ", DUMP_PREFIX_OFFSET,
+		       16, 1, vaddr, 64, true);
+}
+#endif
+
+#ifdef CONFIG_LZERO_NVME_DEBUG
+#define BIG_NUM 999999999
+static atomic_t min_latency = ATOMIC_INIT(BIG_NUM);
+#endif
+
+static void bio_end_io(struct bio *bio)
+{
+	llfree_result_t res;
+	struct bio_ctx *ctx = (struct bio_ctx *)bio->bi_private;
+	struct zone *zone = ctx->zone;
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+	ktime_t end;
+	uint32_t dur, curr_min, min;
+#endif
+
+	if (bio->bi_status != BLK_STS_OK) {
+		pr_info("bio_end_io: Read failed with status %d\n",
+			bio->bi_status);
+		BUG_ON(bio->bi_status);
+	}
+
+	atomic_dec(&bio_inflight);
+
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+	_dump_page(ctx->offset, ctx->frame);
+
+	end = ktime_get();
+	dur = (uint32_t)ktime_to_ns(ktime_sub(end, ctx->start));
+
+	curr_min = atomic_read(&min_latency);
+	min = min(dur, curr_min);
+	atomic_set(&min_latency, min);
+#endif
+	if (unlikely(ctx->benchmark)) {
+		uint32_t cnt = atomic_inc_return(&completion_ctr);
+		/* uint32_t ctr = atomic_inc_return(&endio_ctr); */
+		/* pr_info("bio endio ctr: %d", ctr); */
+
+		if (cnt > bench_num_pages) {
+			pr_err("WHAT?!!!!\n");
+		}
+
+		if (cnt == bench_num_pages) {
+			wake_up(&bench_waitq);
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+			pr_info("Min latency: %d ns\n", dur); // DEBUG
+			atomic_set(&min_latency, BIG_NUM);
+#endif
+			pr_info("Benchmark completed: %zu ops\n",
+				bench_num_pages);
+		}
+		/* res = llfree_put(zone->llfree_dirty, zone->llfree_dirty, */
+		/* 		 ctx->cpu, ctx->frame, llflags(ctx->order)); */
+	} else {
+		// Free the entire huge page
+		res = llfree_return(zone->llfree, res.frame, true);
+		BUG_ON(res.error);
+	}
+
+	kfree(ctx);
+	bio_put(bio);
+}
+
+static bool llzero_page_cpu(struct zone *zone, optional_size_t bench_frame)
+{
+	size_t offset = ALIGN_DOWN(zone->zone_start_pfn, 1 << MAX_ORDER);
+	struct page *page = NULL;
+	llfree_result_t res;
+
+	if (bench_frame.present) {
+		zone = bench_zone;
+		res = llfree_ok(bench_frame.value, false, false);
+	} else {
 		int cpu = get_cpu();
-
 		res = llfree_reclaim(zone->llfree, cpu, true, true, true);
 		put_cpu();
 		if (!llfree_is_ok(res)) {
@@ -66,19 +239,135 @@ static bool llzero_pages(struct zone *zone)
 			return false;
 		}
 		BUG_ON(res.zeroed);
+	}
 
-		page = pfn_to_page(offset + res.frame);
+	page = pfn_to_page(offset + res.frame);
 #ifdef CONFIG_LLZERO_NT
-		memset_nt(page_address(page), 0,
-			  PAGE_SIZE << LLFREE_HUGE_ORDER);
+	memset_nt(page_address(page), 0, PAGE_SIZE << LLFREE_HUGE_ORDER);
 #else
-		__memset(page_address(page), 0, PAGE_SIZE << LLFREE_HUGE_ORDER);
+	__memset(page_address(page), 0, PAGE_SIZE << LLFREE_HUGE_ORDER);
 #endif
 
+	if (unlikely(bench_frame.present)) {
+		int cnt = atomic_inc_return(&completion_ctr);
+		if (cnt == bench_num_pages) {
+			wake_up(&bench_waitq);
+			pr_info("Benchmark completed: %zu ops\n",
+				bench_num_pages);
+		}
+	} else {
 		res = llfree_return(zone->llfree, res.frame, true);
 		BUG_ON(!llfree_is_ok(res));
 	}
-	pr_info("Zeroed %zd pages in zone %s\n", pages_to_zero, zone->name);
+	return true;
+}
+
+/// Gets page from llfree and send to ssd to zero.
+static bool llzero_page_ssd(struct zone *zone, optional_size_t bench_frame)
+{
+	size_t cpu;
+	struct bio_ctx *ctx;
+	struct bio *bio;
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+	ktime_t start;
+#endif
+	int ret;
+	size_t offset;
+	struct page *page = NULL;
+	llfree_result_t res;
+	uint32_t curr_sector, curr_inflight;
+
+	curr_inflight = atomic_inc_return(&bio_inflight);
+	if (curr_inflight > iodepth) {
+		atomic_dec(&bio_inflight);
+		return false;
+	}
+
+	if (bench_frame.present) {
+		zone = bench_zone;
+		res = llfree_ok(bench_frame.value, false, false);
+		cpu = bench_cpu;
+	} else {
+		cpu = get_cpu();
+		put_cpu();
+		res = llfree_reclaim(zone->llfree, cpu, true, true, true);
+		if (res.error) {
+			atomic_dec(&bio_inflight);
+			return false;
+		}
+	}
+
+	offset = ALIGN_DOWN(zone->zone_start_pfn, 1 << MAX_ORDER);
+
+	page = pfn_to_page(offset + res.frame);
+
+	ctx = kzalloc(sizeof(struct bio_ctx), GFP_KERNEL);
+	ctx->zone = zone;
+	ctx->frame = res.frame;
+	ctx->order = HUGETLB_PAGE_ORDER;
+	ctx->cpu = cpu;
+	ctx->offset = offset;
+	ctx->benchmark = bench_frame.present;
+
+	if (bench_const_sector) {
+		curr_sector = 0;
+	} else {
+		curr_sector = atomic_inc_return(&blk_2mib_ctr);
+		curr_sector =
+			(curr_sector % BLK_2MIB_COUNT) * SECTOR_OFFSET_2MIB;
+	}
+
+	bio = bio_alloc_bioset(bdev, 1, REQ_OP_READ, GFP_KERNEL, &bs);
+
+	// NOTE: The nvme partition to give needs to be empty from
+	//       the first sector!!!!
+	bio->bi_iter.bi_sector = curr_sector;
+	bio->bi_end_io = bio_end_io;
+	bio->bi_private = (void *)ctx;
+
+	ret = bio_add_page(bio, page, PAGE_SIZE << HUGETLB_PAGE_ORDER, 0);
+	if (!ret) {
+		pr_err("Failed to add page to bio.");
+		kfree(ctx);
+		bio_put(bio);
+		if (!bench_frame.present) {
+			llfree_result_t res2;
+			res2 = llfree_put(zone->llfree, 0, res.frame,
+					  llflags(HUGETLB_PAGE_ORDER));
+			BUG_ON(res2.error);
+		}
+		atomic_dec(&bio_inflight);
+		return false;
+	}
+
+#ifdef CONFIG_LLZERO_NVME_DEBUG
+	start = ktime_get();
+	ctx->start = start;
+#endif
+
+	submit_bio(bio);
+	return true;
+}
+
+/// Returns true if only zero percent was zeroed but there are still pages to zero.
+static bool llzero_pages(struct zone *zone, bool ssd)
+{
+	size_t pages_to_zero = num_pages_to_zero(zone);
+	// Ensure we do not zero more than the limit
+	size_t exceeds_limit = pages_to_zero > huge_page_limit();
+	pages_to_zero = min_t(size_t, pages_to_zero, huge_page_limit());
+
+	for (size_t i = 0; i < pages_to_zero; i++) {
+		bool success;
+		if (ssd)
+			success = llzero_page_ssd(zone, optional_size_none());
+		else
+			success = llzero_page_cpu(zone, optional_size_none());
+		if (!success)
+			return true;
+	}
+	if (pages_to_zero > 0)
+		pr_info("Zeroed %zd pages in zone %s\n", pages_to_zero, zone->name);
 
 	return exceeds_limit;
 }
@@ -87,39 +376,154 @@ static int llzero_task(void *data)
 {
 	struct zone *zone = (struct zone *)data;
 
-#ifdef CONFIG_LLZERO_BENCH
-	ktime_t start, end;
-	bool once = true;
-#endif
 	usleep_range(delay, 8 * delay);
 
-#ifdef CONFIG_LLZERO_BENCH
-	start = ktime_get();
-#endif
-
 	while (!kthread_should_stop()) {
-		bool zeroes_left = llzero_pages(zone);
-
+		size_t d = delay;
+		bool zeroes_left = llzero_pages(zone, bdev);
 		// Sleep longer if there are no pages left to zero
-		size_t d = delay * (zeroes_left ? 1 : 8);
-		// TODO: Remove this check!
-		// llfree_validate(zone->llfree);
-
-#ifdef CONFIG_LLZERO_BENCH
-		if (once && (strcmp(zone->name, "Normal") == 0)) {
-			s64 delta;
-			end = ktime_get();
-			delta = ktime_to_ns(ktime_sub(end, start));
-			pr_info("First zeroing %s took: %lld ms\n", zone->name,
-				delta / 1000000);
-			once = false;
-		}
-#endif
-
+		d *= (zeroes_left ? 1 : 8);
 		usleep_range(d - (d / 10), d + (d / 10));
 	}
 	return 0;
 }
+
+static int device_set(const char *val, const struct kernel_param *kp)
+{
+	int res = param_set_charp(val, kp);
+	if (res < 0) {
+		pr_err("Failed to set device: %s\n", val);
+		return res;
+	}
+	if (bdev != NULL) {
+		// Wait for all bios to finish
+		while (atomic_read(&bio_inflight) != 0) {
+			msleep(1);
+		}
+		blkdev_put(bdev, mode);
+		bdev = NULL;
+	}
+	if (*val == '\0') {
+		pr_info("No device specified, using cpu.\n");
+		return 0;
+	}
+	bdev = blkdev_get_by_path(val, mode, THIS_MODULE);
+	if (IS_ERR(bdev)) {
+		int err = PTR_ERR(bdev);
+		pr_err("Failed to open block device %s: %d\n", val, err);
+		bdev = NULL;
+		return err;
+	}
+	pr_info("Opened block device %s\n", val);
+	return res;
+}
+
+static long async_zero_ioctl(struct file *filp, unsigned int cmd,
+			     unsigned long arg)
+{
+	void __user *argp = (void __user *)arg;
+
+	ktime_t start, end;
+	struct benchmark_args args;
+	size_t cpu;
+	u64 *bench_pages;
+
+	llflags_t llf = llflags(LLFREE_HUGE_ORDER);
+	llf.movable = 1;
+
+	if (cmd != ZERO_IOCTL_BENCHMARK) {
+		pr_err("Invalid ioctl command: %u\n", cmd);
+		return -EINVAL;
+	}
+	if (copy_from_user(&args, argp, sizeof(args))) {
+		return -EFAULT;
+	}
+
+	// get normal zone
+	for_each_populated_zone(bench_zone) {
+		if (!strcmp(bench_zone->name, "Normal")) {
+			break;
+		}
+	}
+	cpu = __smp_processor_id();
+
+	bench_cpu = cpu;
+	bench_num_pages = args.num_pages;
+
+	iodepth = args.iodepth;
+	bench_const_sector = args.const_iosector;
+
+	if (enabled) {
+		pr_err("Cannot run benchmark zeroing is enabled.\n");
+		return -EINVAL;
+	}
+
+	atomic_set(&completion_ctr, 0);
+
+	// allocate llfree results
+	// INIT_KFIFO(bench_jobs);
+	bench_pages = kcalloc(bench_num_pages, sizeof(u64), GFP_KERNEL);
+	for (int i = 0; i < bench_num_pages; i++) {
+		llfree_result_t res = llfree_get(bench_zone->llfree, cpu, llf);
+		BUG_ON(!llfree_is_ok(res));
+		bench_pages[i] = res.frame;
+	}
+
+	pr_info("running benchmark..\n");
+	start = ktime_get();
+
+	// start zeroing operation
+	for (int i = 0; i < bench_num_pages; i++) {
+		// TODO: Support cpu zeroing
+		bool issued = llzero_page_ssd(bench_zone,
+					      optional_size(bench_pages[i]));
+		if (!issued) {
+			size_t d = delay;
+			usleep_range(d - (d / 10), d + (d / 10));
+		}
+	}
+
+	wait_event(bench_waitq,
+		   atomic_read(&completion_ctr) == bench_num_pages);
+	end = ktime_get();
+	pr_info("benchmark done.\n");
+
+	args.duration_ns = ktime_to_ns(ktime_sub(end, start));
+
+	if (copy_to_user(argp, &args, sizeof(args))) {
+		return -EFAULT;
+	}
+
+	// Free llfree results
+	for (int i = 0; i < bench_num_pages; i++) {
+		llfree_result_t res;
+		res = llfree_put(bench_zone->llfree, cpu, bench_pages[i], llf);
+		BUG_ON(!llfree_is_ok(res));
+	}
+	kfree(bench_pages);
+
+	atomic_set(&completion_ctr, 0);
+	iodepth = CONFIG_LLZERO_IODEPTH;
+	bench_const_sector = false;
+
+	kfree(zero_tasks);
+	zero_tasks = NULL;
+	zero_tasks_len = 0;
+
+	msleep(500);
+
+	return 0;
+}
+
+struct file_operations fops = {
+	.unlocked_ioctl = async_zero_ioctl,
+};
+
+static const struct kernel_param_ops device_ops = {
+	.set = device_set,
+	.get = param_get_charp,
+};
+module_param_cb(device, &device_ops, &device, 0644);
 
 static int start_zero_tasks(void)
 {
@@ -161,6 +565,11 @@ static int stop_zero_tasks(void)
 	if (!zero_tasks)
 		return 0;
 
+	// Wait for all bios to finish
+	while (atomic_read(&bio_inflight) != 0) {
+		msleep(1);
+	}
+
 	for (size_t i = 0; i < zero_tasks_len; i++) {
 		if (zero_tasks[i]) {
 			kthread_stop(zero_tasks[i]);
@@ -184,6 +593,11 @@ static int enabled_set(const char *val, const struct kernel_param *kp)
 	if (!initialized)
 		return 0;
 
+	if (bench_num_pages > 0) {
+		pr_err("Cannot change enabled state while benchmark is running.\n");
+		return -EBUSY;
+	}
+
 	if (enabled) {
 		return start_zero_tasks();
 	} else {
@@ -202,11 +616,29 @@ module_param(percentage, uint, 0664);
 module_param(limit, uint, 0664);
 module_param(delay, uint, 0664);
 
+#define CDEV_NAME "async-zero"
+
 static int __init llzero_init(void)
 {
 	initialized = true;
+
+	major = register_chrdev(0, CDEV_NAME, &fops);
+	if (major < 0) {
+		pr_alert("Registering char device failed with %d\n", major);
+		return major;
+	}
+
+	cls = class_create(THIS_MODULE, CDEV_NAME);
+	device_create(cls, NULL, MKDEV(major, 0), NULL, CDEV_NAME);
+
+	pr_info("Device created on /dev/%s\n", CDEV_NAME);
+
+	if (bioset_init(&bs, LLZERO_BIO_POOL_SIZE, 0, BIOSET_NEED_BVECS))
+		panic("llzero: can't allocate bios\n");
+
 	if (enabled)
 		return start_zero_tasks();
+
 	return 0;
 }
 
@@ -215,6 +647,16 @@ static void __exit llzero_exit(void)
 	int ret = stop_zero_tasks();
 	if (ret < 0)
 		pr_err("Failed to stop zero tasks: %d\n", ret);
+
+	bioset_exit(&bs);
+
+	if (bdev) {
+		blkdev_put(bdev, mode);
+	}
+	device_destroy(cls, MKDEV(major, 0));
+	class_destroy(cls);
+	unregister_chrdev(major, CDEV_NAME);
+
 	initialized = false;
 }
 
